f8544fe5d80d1c011e7d1b6b08aa9169fd3c6be7
NeMi <tmdals757@gmail.com>
2019-11-22 15:07:22 +0900

    Food Thread Added

--- Source_code/Game/Snake/snake.py
+++ Source_code/Game/Snake/snake.py
@@ -1,5 +1,6 @@
 import keyboard as kbd
 import random as rd
+import threading
 import time
 import os
 
@@ -10,6 +11,10 @@ class EscInput(Exception) :                         # 종료 입력
         pass
 
 class MapOverAccess(Exception) :                    # 구역 지정
+    def __init__(self) :
+        pass
+
+class BodyTouch(Exception) :
     def __init__(self) :
         pass
         
@@ -30,31 +35,43 @@ class Snake :
         W_x, W_y = self.WAY[head]
         if not 0 < x+W_x < 15 or not 0 < y+W_y < 26 : # 맵 초과 접근 에러
             raise MapOverAccess
+        elif (x+W_x, y+W_y) in self.pos :
+            raise BodyTouch
         self.pos.append((x+W_x, y+W_y))              # 몸통은 유지, 머리를 갱신
         self.bodypart = self.pos.pop(0)
 
     def change_head(self, e) :                      # 머리 방향
         for code in kbd._pressed_events :
-            if code == 17 or code == 72 :
+            if (code == 17 or code == 72) and self.dir != 2:
                 self.dir = 1
-            elif code == 31 or code == 80 :
+            elif (code == 31 or code == 80) and self.dir != 1:
                 self.dir = 2
-            elif code == 30 or code == 75 :
+            elif (code == 30 or code == 75) and self.dir != 4:
                 self.dir = 3
-            elif code == 32 or code == 77 :
+            elif (code == 32 or code == 77) and self.dir != 3:
                 self.dir = 4
             elif code == 1 :
                 self.dir = -1
 
-class Food :
+class Food() :
     def __init__(self) :
-        self.pos = (rd.randint(1, 14), rd.randint(1, 25))
+        self.pos = []
+
+    def thread_food(self) :                             # 먹이를 추가하는 함수
+        while True :
+            (x, y) = ((rd.randint(1, 14), rd.randint(1, 25)))
+            if not self.chk_food((x, y)) :
+                self.set_food((x, y))
+            time.sleep(10)
 
     def set_food(self, rd_pos) :
-        self.pos = rd_pos
+        self.pos.append(rd_pos)
 
-    def chk_food(self, A, B) :
-        return A == B
+    def remove_food(self, rm_pos) :
+        self.pos.remove(rm_pos)
+
+    def chk_food(self, chk_pos) :
+        return chk_pos in self.pos
     
 class Map :
     N = 16
@@ -70,19 +87,20 @@ class Map :
                 self.place_food()
         self.food.set_food((x, y))
 
-    def print_map(self) :                           # 테스트 전용 출력 방식
+    def print_map(self) :                           # -- 테스트 전용 출력 방식 -- #
         for i in range(self.N) :
             for j in range(self.M) :
                 if i % 15 == 0 or j % 26 == 0 :
                     print('■', end='')
                 elif (i, j) == self.snake.pos[-1] :
                     print(self.snake.LIST[self.snake.dir], end='')
-                    if(self.food.chk_food((i, j), self.food.pos)) :
+                    if self.food.chk_food((i, j)) :
                         self.snake.pos.insert(0, self.snake.bodypart)
                         self.place_food()
+                        self.food.remove_food((i, j))
                 elif (i, j) in self.snake.pos :
                     print('●', end='')
-                elif (i, j) == self.food.pos :
+                elif (i, j) in self.food.pos :
                     print('★', end='')
                 else :
                     print('□', end='')
@@ -91,9 +109,12 @@ class Map :
 # __main__ #
 
 if __name__ == '__main__' :
-
+    
     S = Snake()
     M = Map(S)
+    t = threading.Thread(target=M.food.thread_food, args=())    # 먹이 증가 스레드 생성
+    t.setDaemon(True)                                           # 메인 프로세스와 함께 종료
+    t.start()
 
     try :
         while True :
@@ -102,9 +123,11 @@ if __name__ == '__main__' :
             if S.dir == -1 :
                 raise EscInput
             M.print_map()
-            time.sleep(0.05)
+            time.sleep(0.1)
             os.system('cls' if os.name == 'nt' else 'clear')
-    except MapOverAccess :
+    except MapOverAccess :                                      # -- 테스트용 에러 메세지 출력 -- #
         print('맵을 벗어났습니다.')
+    except BodyTouch :
+        print('몸에 닿았습니다.')
     except EscInput :
         print('ESC가 입력되어 게임을 종료합니다.')
